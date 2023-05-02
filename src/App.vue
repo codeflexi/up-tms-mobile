c0eb611f8120cc15bf090bb8b29b48ce78722d07
Nattapong M <nattapong.m@tcrbank.com>
2023-05-02 22:21:09 +0700

    adding consolelog

--- src/App.vue
+++ src/App.vue
@@ -16,21 +16,21 @@ const router = useRouter();
 const userStore = useUserStore();
 
 onMounted(async () => {
-  // server.interceptors.response.use(
-  //   (response) => response,
-  //   (error) => {
-  //     if (error.response.status === 401) {
-  //       $q.dialog({
-  //         message:
-  //           "Your login was unsuccessful. Please make sure that you are connected to the internet." +
-  //           error.response.data.error,
-  //       });
-  //       // userStore.clearUser()
-  //       router.push("/auth");
-  //       // console.log("IS EXPIRED hey hey!!!", error.response.status);
-  //       // console.log("Error hey hey!!!", error.response.data.error);
-  //     }
-  //   }
-  // );
+  server.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.status === 401) {
+        $q.dialog({
+          message:
+            "Your login was unsuccessful. Please make sure that you are connected to the internet." +
+            error.response.data.error,
+        });
+        // userStore.clearUser()
+        router.push("/auth");
+        // console.log("IS EXPIRED hey hey!!!", error.response.status);
+        // console.log("Error hey hey!!!", error.response.data.error);
+      }
+    }
+  );
 });
 </script>
